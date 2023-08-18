91379163112cf449467ba58f4f3d0956ba1621e7
grace-ghb <>
2023-08-18 21:40:59 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -3,7 +3,6 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -36,7 +35,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: \n")
         # print(f"The data provided is {data_str}") - This code is to confirm it's working
         sales_data = data_str.split(",")
         validate_data(sales_data)
