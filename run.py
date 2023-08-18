8ddd65d95f5fc769a9bf05ec146017dce45f6734
grace-ghb <>
2023-08-18 20:16:49 +0000

    Collect sales data for last 5 markets in lists of colums

--- run.py
+++ run.py
@@ -66,6 +66,7 @@ def validate_data(values):
     
     return True
 
+
 """
 def update_sales_worksheet(data):
     
@@ -86,6 +87,7 @@ def update_surplus_worksheet(data):
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
 """
+
 
 def update_worksheet(data, worksheet):
     """
@@ -118,6 +120,24 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    # column = sales.col_values(3)
+    # print(column)
+
+    columns = []
+    for ind in range(1, 7):
+        # print(ind)
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -131,4 +151,6 @@ def main():
 
 
 print("WElcome to Lova Sandwishes Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
